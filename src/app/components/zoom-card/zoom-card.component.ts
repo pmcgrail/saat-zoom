76f85d29597a6cd70e69d85b1404db030d257e6e
mcgrailp <mcgrail.p@gmail.com>
2020-09-16 21:29:38 -0400

    Add countdown to zoom cards

--- src/app/components/zoom-card/zoom-card.component.ts
+++ src/app/components/zoom-card/zoom-card.component.ts
@@ -10,4 +10,20 @@ import { IZoomCall } from '../../models';
 })
 export class ZoomCardComponent {
   @Input() call: IZoomCall;
+
+  get countdown() {
+    const diff = new Date(this.call.datetime).getTime() - new Date().getTime();
+    const hours = diff / (1000 * 60 * 60);
+    const minutes = diff / (1000 * 60);
+    if (diff > 0) {
+      return hours > 24
+        ? `Call starts in ${Math.round(hours / 24)} day(s)`
+        : hours > 1
+        ? `Call starts in ${Math.floor(hours)} hour(s)`
+        : `Call starts in ${Math.floor(minutes)} minute(s)`;
+    }
+    return hours > 1
+      ? `Call started ${-Math.floor(hours)} hour(s) ago`
+      : `Call started ${-Math.floor(minutes)} minute(s) ago`;
+  }
 }
